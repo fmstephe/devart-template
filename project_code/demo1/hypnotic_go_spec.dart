ec7701c5f4c08d19ec5a36e9a0797b3c69057e92
Francis Stephens <francisstephens@gmail.com>
2014-02-11 13:43:46 +0000

    Added Demo1 page showing first stage
    Here we can host the first iteration of this project. This avoids
    needing to create youtube videos or take pictures. We can just link to
    working code.

---
+++ project_code/demo1/hypnotic_go_spec.dart
@@ -0,0 +1,28 @@
+import 'dart:html';
+import 'gotext.dart';
+import 'dart:async';
+
+void main() {
+  SpecText gospec = new SpecText();
+  print(gospec.nextChar());
+  print(gospec.nextChar());
+  print(gospec.nextChar());
+  print(gospec.nextChar());
+  print(gospec.nextChar());
+  print(gospec.nextChar());
+  var timer = new Timer.periodic(new Duration(seconds:1), (Timer timer) => nextLetter(gospec));
+}
+
+void nextLetter(SpecText gospec) {
+  var txt = querySelector("#text_canvas");
+  var ctxt = txt.getContext("2d");
+  var hgt = txt.height;
+  var wdt = txt.width;
+  ctxt.fillStyle = "rgba(255, 255, 255, 1)";
+  ctxt.fillRect(0,0,wdt,hgt);
+  ctxt.fillStyle = "rgba(0, 0, 0, 1)";
+  ctxt.font = "bold " + hgt.toString() + "px sans-serif";
+  ctxt.textAlign = "center";
+  ctxt.textBaseline = "middle";
+  ctxt.fillText(gospec.nextChar(), wdt/2, hgt/2 - 5);
+}

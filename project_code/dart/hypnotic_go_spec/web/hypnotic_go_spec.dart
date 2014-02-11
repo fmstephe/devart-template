6e68af82882047dbd0fb6e13b66e6a2f21ae7add
Francis Stephens <francisstephens@gmail.com>
2014-02-11 12:57:08 +0000

    Added particles to the main canvas.
    The main canvas now animates 10,000 particles randomly. The text canvas
    is hidden and is not being updated. Will be utilised later.
    
    Also removed the generated javascript source files from the git repo.

--- project_code/dart/hypnotic_go_spec/web/hypnotic_go_spec.dart
+++ project_code/dart/hypnotic_go_spec/web/hypnotic_go_spec.dart
@@ -1,28 +1,75 @@
 import 'dart:html';
 import 'gotext.dart';
+import 'particle.dart';
 import 'dart:async';
+import 'dart:math';
+
+List<Particle> particles;
+num lastStamp = 0.0;
 
 void main() {
+  CanvasElement main = querySelector("#main_canvas");
+  int width = main.width;
+  int height = main.height;
+  CanvasRenderingContext2D mainCtxt = main.getContext("2d");
+  mainCtxt.fillStyle = "rgba(0, 0, 0, 1)";
+  mainCtxt.fillRect(0,0,width,height);
   SpecText gospec = new SpecText();
-  print(gospec.nextChar());
-  print(gospec.nextChar());
-  print(gospec.nextChar());
-  print(gospec.nextChar());
-  print(gospec.nextChar());
-  print(gospec.nextChar());
-  var timer = new Timer.periodic(new Duration(seconds:10), (Timer timer) => nextLetter(gospec));
+  particles = makeParticles(width, height);
+  Stopwatch watch = new Stopwatch();
+  watch.start();
+//  var timerTxt = new Timer.periodic(new Duration(seconds:1), (Timer timer) => nextLetter(gospec));
+  window.animationFrame.then(loop);
+}
+
+List<Particle> makeParticles(int width, int height) {
+  List<Particle> particles = new List<Particle>();
+  for (int i = 0; i < 10000; i++) {
+    particles.add(new Particle(width, height));
+  }
+  return particles;
 }
 
 void nextLetter(SpecText gospec) {
-  var txt = querySelector("#text_canvas");
-  var ctxt = txt.getContext("2d");
-  var hgt = txt.height;
-  var wdt = txt.width;
-  ctxt.fillStyle = "rgba(255, 255, 255, 1)";
-  ctxt.fillRect(0,0,wdt,hgt);
-  ctxt.fillStyle = "rgba(0, 0, 0, 1)";
-  ctxt.font = "bold " + hgt.toString() + "px sans-serif";
-  ctxt.textAlign = "center";
-  ctxt.textBaseline = "middle";
-  ctxt.fillText(gospec.nextChar(), wdt/2, hgt/2 - 5);
+  CanvasElement txt = querySelector("#text_canvas");
+  int height = txt.height;
+  int width = txt.width;
+  CanvasRenderingContext2D textCtxt = txt.getContext("2d");
+  textCtxt.fillStyle = "rgba(255, 255, 255, 1)";
+  textCtxt.fillRect(0,0,width,height);
+  textCtxt.fillStyle = "rgba(0, 0, 0, 1)";
+  textCtxt.font = "bold " + height.toString() + "px sans-serif";
+  textCtxt.textAlign = "center";
+  textCtxt.textBaseline = "middle";
+  textCtxt.fillText(gospec.nextChar(), width/2, height/2 - 5);
+}
+
+void loop(num delta) {
+  if (lastStamp == 0.0) {
+    lastStamp = delta;
+  }
+  print(delta - lastStamp);
+  lastStamp = delta;
+  for (Particle particle in particles) {
+    particle.push();
+    particle.move();
+  }
+  render(particles);
+  window.requestAnimationFrame(loop);
+}
+
+void render(List<Particle> particles) {
+  CanvasElement main = querySelector("#main_canvas");
+  int width = main.width;
+  int height = main.height;
+  CanvasRenderingContext2D mainCtxt = main.getContext("2d");
+  mainCtxt.fillStyle = "rgba(0, 0, 0, 0.05)";
+  mainCtxt.fillRect(0,0,width,height);
+  mainCtxt.fillStyle = "rgba(255, 180, 180, 0.1)";
+  for (Particle particle in particles) {
+    mainCtxt.beginPath();
+    mainCtxt.arc(particle.getX(), particle.getY(), 1, 0, PI*2, false);
+    mainCtxt.closePath();
+    mainCtxt.fill();
+  }
 }
